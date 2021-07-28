38321dcd64976abb37a71387670924d9c44bf31a
Bhubanghimire <bhubanghimire3130@gmail.com>
2021-07-28 20:27:43 +0545

    model completed

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-from .models import Vote
+from .models import Vote,Poll,Choice
+
 # Register your models here.
+admin.site.register(Vote)
+admin.site.register(Poll)
+admin.site.register(Choice)
